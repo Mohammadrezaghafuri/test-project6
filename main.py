318fdcef501e8551d564a33e9f44fe3d2b31732b
Mohammadreza Ghafuri <100825523+Mohammadrezaghafuri@users.noreply.github.com>
2022-03-10 19:18:11 +0330

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
-print('Mohammadreza Ghafuri')
-print(98548121)
+n = int(input("number of student :"))
+
+users = []
+
+for e in range(n):
+    student = input('name:')
+    age =input('age:')
+    users.append({'name':student , 'age': age})    
+
+serch_name = input("name for serch:")
+
+for e in users:
+    el = len(users)-1
+   
+    if e['name'] == serch_name:
+        print(e['age'])
+        break
+    elif e['name'] != serch_name:
+          if e == el:
+              print("there is no user with given name")
+          else:
+             continue
+      
+
+     
